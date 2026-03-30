fcb44e02315bdaefe788ef71712c2177c84eddd9
Russell Martin <russell@martinandbrown.com>
2026-03-30 16:26:08 -0400

    Add cron job

---
+++ cron_job.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+cd /Users/martin/Dropbox/Python/spiel-counter
+/Users/martin/.local/bin/uv run post.py
---
+++ post.py
@@ -0,0 +1,18 @@
+from atproto import Client, client_utils
+
+import json
+import datetime
+
+with open('credentials.json', 'r') as f:
+    credentials = json.loads(f.read())
+
+client = Client()
+profile = client.login(credentials['username'], credentials['password'])
+
+target = datetime.datetime(2026, 10, 22, 10)
+today = datetime.datetime.now()
+diff = target - today
+
+text = client_utils.TextBuilder().text(f'It is {diff.days} days until ').link('Spiel 2026', 'https://www.spiel-essen.de/').text(' (starts 22 Oct 2026)!')
+post = client.send_post(text)
+
