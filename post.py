7f677d82f79284b1c04be3341bc0894beba2009f
Russell Martin <russell@martinandbrown.com>
2026-03-31 10:59:52 -0400

    Add code 'guard'

--- post.py
+++ post.py
@@ -3,16 +3,20 @@ from atproto import Client, client_utils
 import json
 import datetime
 
-with open('credentials.json', 'r') as f:
-    credentials = json.loads(f.read())
+def main():
+    with open('credentials.json', 'r') as f:
+        credentials = json.loads(f.read())
 
-client = Client()
-profile = client.login(credentials['username'], credentials['password'])
+    client = Client()
+    profile = client.login(credentials['username'], credentials['password'])
 
-target = datetime.datetime(2026, 10, 22, 10)
-today = datetime.datetime.now()
-diff = target - today
+    target = datetime.datetime(2026, 10, 22, 10)
+    today = datetime.datetime.now()
+    diff = target - today
 
-text = client_utils.TextBuilder().text(f'It is {diff.days} days until ').link('Spiel 2026', 'https://www.spiel-essen.de/').text(' (starts 22 Oct 2026)!')
-post = client.send_post(text)
+    text = client_utils.TextBuilder().text(f'It is {diff.days} days until ').link('Spiel 2026', 'https://www.spiel-essen.de/').text(' (starts 22 Oct 2026)!')
+    post = client.send_post(text)
+
+if __name__ == '__main__':
+    main()
 
